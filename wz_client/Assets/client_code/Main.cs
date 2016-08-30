455fb3125b1eb5cf01ab1f94e0d19cdc7f893cd8
wangzhao <316315963@qq.com>
2016-08-30 10:52:25 +0800

    @init the client

---
+++ wz_client/Assets/client_code/Main.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using WzBuffer;
+
+public class Main : MonoBehaviour {
+    private Socket so = null;
+
+
+    void Awake()
+    {
+        Object obj = Resources.Load("hello", typeof(TextAsset)) as TextAsset;
+
+        ByteBuffer buffer = new ByteBuffer(128);
+
+        buffer.writeByte(-11);
+        buffer.writeChar(70);
+        buffer.writeInt(-1234);
+        buffer.writeLong(13243535L);
+
+
+        int a = buffer.readByte();
+        char b = buffer.readChar();
+        int c = buffer.readInt();
+        long d = buffer.readLong();
+
+        UnityEngine.Debug.Log(a);
+        UnityEngine.Debug.Log(b);
+        UnityEngine.Debug.Log(c);
+        UnityEngine.Debug.Log(d);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
---
+++ wz_client/Assets/client_code/net/buffer/ByteBuffer.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WzBuffer
+{
+    public class ByteBuffer
+    {
+
+        private byte[] bytes = null;
+
+        private int writerIndex = 0;
+
+        private int readerIndex = 0;
+
+        public ByteBuffer(int capicity , ByteOrder order)
+        {
+            bytes = new byte[capicity];
+        }
+
+        public ByteBuffer(int capicity):this(capicity , ByteOrder.BIG)
+        {
+           
+        }
+
+        public  int capicity()
+        {
+            return bytes.Length;
+        }
+
+        private void ensureWriteable(int length)
+        {
+            int minWriteAble = length + writerIndex;
+            if( minWriteAble < capicity())
+            {
+                return;
+            }
+            int newCapacity = 64;
+            while(newCapacity < minWriteAble)
+            {
+                newCapacity <<= 1;
+            }
+        }
+
+
+
+        public  void writeByte(int value)
+        {
+            ensureWriteable(1);
+            HeapByteBufferUtil.setByte(bytes, writerIndex, value);
+            writerIndex++;
+        }
+
+        public void writeBytes(byte[] dst)
+        {
+            int length = dst.Length;
+            ensureWriteable(length);
+
+        }
+
+        public  void writeInt(int value)
+        {
+            ensureWriteable(4);
+            HeapByteBufferUtil.setInt(bytes, writerIndex, value);
+            writerIndex += 4;
+        }
+
+        public void writeBoolean(bool value)
+        {
+            ensureWriteable(1);
+            HeapByteBufferUtil.setBoolean(bytes, writerIndex, value);
+            writerIndex++;
+        }
+
+        public void writeDouble(double value) {
+
+        }
+
+        public  void writeShort(int value)
+        {
+            ensureWriteable(2);
+            HeapByteBufferUtil.setShort(bytes, writerIndex, value);
+            writerIndex += 2;
+        }
+
+        public  void writeUtf8(string value)
+        {
+            if( value == null)
+            {
+                writeShort(0);
+            }
+            else
+            {
+                byte[] data = Encoding.UTF8.GetBytes(value);
+                short length = (short)data.Length;
+                writeShort(length);
+                writeBytes(data);
+            }
+ 
+        }
+
+        public  void writeFloat(float value)
+        {
+
+        }
+
+        public void writeChar(int value)
+        {
+            ensureWriteable(2);
+            HeapByteBufferUtil.setChar(bytes, writerIndex, value);
+            writerIndex += 2;
+        }
+
+        public void writeLong(long value)
+        {
+            ensureWriteable(8);
+            HeapByteBufferUtil.setLong(bytes, writerIndex, value);
+            writerIndex += 8;
+        }
+
+
+        public int readerAbleBytes()
+        {
+            return capicity() - readerIndex;
+        }
+
+        public  int readByte()
+        {
+            byte value = HeapByteBufferUtil.getByte(bytes, readerIndex);
+            readerIndex++;
+            return value;
+        }
+
+        public  short readShort()
+        {
+            short value = HeapByteBufferUtil.getShort(bytes, readerIndex);
+            readerIndex += 2;
+            return value;
+        }
+
+        public  int readInt()
+        {
+            int value = HeapByteBufferUtil.getInt(bytes, readerIndex);
+            readerIndex += 4;
+            return value;
+        }
+
+        public  long readLong()
+        {
+            long value = HeapByteBufferUtil.getLong(bytes, readerIndex);
+            readerIndex += 8;
+            return value;
+        }
+
+        public  double readDouble()
+        {
+            return 0;
+        }
+
+        public  float readFloat()
+        {
+            return 0;
+        }
+
+        public  string readString()
+        {
+            return "1111";
+        }
+
+        public  char readChar()
+        {
+            char value = HeapByteBufferUtil.getChar(bytes, readerIndex);
+            readerIndex += 2;
+            return value;
+        }
+    }
+
+    public enum ByteOrder {
+        BIG,
+        LITTLE
+    }
+
+}

db845ab75ba473f32dc72d20b26b835eeab947b2
wz <316315963@qq.com>
2017-09-13 18:03:15 +0800

    客户端界面

---
+++ godsg/Assets/client_code/interface/UiTab.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiTab : MonoBehaviour {
+
+    public Toggle[] toggleList;
+
+    public GameObject[] objectList;
+    
+    // Use this for initialization
+
+    private void Awake()
+    {
+        for(int i = 0, length = toggleList.Length; i < length; i++)
+        {
+            toggleList[i].onValueChanged.AddListener(OnToggleValueChanged);
+        }
+    }
+
+
+    void OnToggleValueChanged(bool isOn)
+    {
+        if(!isOn)
+        {
+            return;
+        }
+        int index = 0;
+        for (int i = 0, length = toggleList.Length; i < length; i++)
+        {
+            if (toggleList[i].isOn)
+            {
+                index = i;
+            }
+        }
+        for (int i = 0, length = objectList.Length; i < length; i++)
+        {
+            if (i != index)
+                objectList[i].SetActive(false);
+            else
+                objectList[i].SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
